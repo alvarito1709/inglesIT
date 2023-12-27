40f0f64e52d779ea1e46cc7659c742cba0f1a099
Alvaro <alvaro9943310@gmail.com>
2023-12-27 12:57:12 -0300

    primer commit

---
+++ inglesIT/src/main/java/com/agencia/inglesIT/InglesParaElTrabajoApplication.java
@@ -0,0 +1,13 @@
+package com.agencia.inglesIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InglesParaElTrabajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InglesParaElTrabajoApplication.class, args);
+	}
+
+}
---
+++ inglesIT/src/test/java/com/agencia/inglesIT/InglesParaElTrabajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agencia.inglesIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InglesParaElTrabajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
