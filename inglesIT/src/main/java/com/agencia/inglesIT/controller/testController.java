1b7300acc3d5cccf2d82163fd5d60d49a00dc526
Alvaro <alvaro9943310@gmail.com>
2024-01-22 16:35:57 -0300

    Arreglo de securityConfig y controller para modelar la vista de test dependiendo del nivel

--- inglesIT/src/main/java/com/agencia/inglesIT/controller/testController.java
+++ inglesIT/src/main/java/com/agencia/inglesIT/controller/testController.java
@@ -1,8 +1,12 @@
 package com.agencia.inglesIT.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/test")
@@ -10,7 +14,16 @@ public class testController {
 
     @GetMapping("/level1")
     String level1(){
-        return "test1";
+        return "testDisplay";
+    }
+
+    @PostMapping("/level")
+    ModelAndView level( Model model,
+            @RequestParam(value = "prueba") String prueba
+    ){
+        model.addAttribute("prueba", prueba);
+
+    return new ModelAndView("testDisplay :: test");
     }
 
     @GetMapping("/init")
