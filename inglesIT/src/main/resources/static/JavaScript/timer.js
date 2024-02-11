78c8cf696d4f8e0472527b5655551683e38313e5
Alvaro <alvaro9943310@gmail.com>
2024-02-11 20:06:59 -0300

    añadido worker para cronometro en segundo plano

--- inglesIT/src/main/resources/static/JavaScript/timer.js
+++ inglesIT/src/main/resources/static/JavaScript/timer.js
@@ -1,41 +1,73 @@
 var hour = 0;
 var minute = 10;
 var second = 60;
-var mSecond = 1000;
 
 
-function initializateTimer(){
-     setInterval(timer, mSecond);
+var interval;
+
+
+self.addEventListener('message', function (event){
+    setUpTimer(event.data);
+})
+
+function setUpTimer(mSecond){
+
+    interval  = setInterval(timer, mSecond);
+
+    function timer(){
+        if (second !== 0){
+            if (second >= 11){
+                if (minute <=1){
+                    second -=1;
+                    self.postMessage({minute: minute, second: second,color: "red"});
+                }
+                else{
+                    second -=1;
+                    self.postMessage({minute: minute, second: second});
+                }
+
+            }
+            else {
+                if (minute <=1){
+                    second -=1;
+                    self.postMessage({minute: minute, second: "0" + second,color: "red"});
+                }
+                else{
+                    second -=1;
+                    self.postMessage({second: "0" + second, minute: minute})
+
+                }
+
+            }
+
+        }
+
+
+        if (second === 0){
+            if(minute === 0){
+                clearInterval(interval);
+                self.postMessage({minute: minute, second: second, finished: true})
+            }
+            else{
+                minute -= 1;
+                second = 60;
+            }
+        }
+
+
+
+
+        console.log(minute + ":" + second);
+    }
+
 }
 
 
 
-function timer(){
-    if (second !== 0){
-        second -=1;
-        document.getElementById("second").innerHTML = second;
-    }
-
-    if (second <10){document.getElementById("second").innerHTML = "0" + second;}
-
-    if (second === 0){
-        if(minute === 0){
-            clearInterval(interval);
-
-            //llamar aca a funciones para guardar los datos al acabarse el tiempo
-        }
-        else{
-            minute -= 1;
-            second = 60;
-        }
-    }
-    document.getElementById("minute").innerHTML = minute;
-
-    if (minute <=1){
-        document.getElementById("counterDisplay").style.color = "red";
-    }
 
 
-  //  return console.log(minute + ":" + second);
 
-}
+
+
+
+
